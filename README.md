48e09fde138d79104001ae591d507cea6ba6e70b
Julien Geneste <julien@aircslab.fr>
2023-04-29 16:52:38 +0200

    added small house world

--- README.md
+++ README.md
@@ -20,7 +20,18 @@ cd ../
 colcon build --symlink-install
 source install/setup.bash
 
-ros2 launch vacuum_bot rsp.launch.pyvac
+ros2 launch vacuum_bot launch_sim.launch.py
 ```
 
 If you now open RViz you should see the robot.
+
+_Note : It's possible that Gazebo can't find models. In this cases, copy models' folder to `vacuum_ws/install/vacuum_bot/share/vacuum_bot/models` :_
+
+```bash
+cp -r models ~/vacuum_ws/install/vacuum_bot/share/vacuum_bot
+```
+
+_Note 2 : Gazebo is experiencing some weird issues. If you get this error : `gzclient: /usr/include/boost/smart_ptr/shared_ptr.hpp:734:typename boost::detail::sp_member_access<T>::type boost::shared_ptr<T>::operator->() const [with T = gazebo::rendering::Camera; typename boost::detail::sp_member_access<T>::type gazebo::rendering::Camera*]: Assertion 'px != 0' failed.`, you should `source /usr/share/gazebo/setup.sh`._
+
+## Playing
+Since you have launched the simulation you could control the robot by launching teleop : `ros2 run teleop_twist_keyboard teleop_twist_keyboard`.
--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -3,9 +3,14 @@ import os
 from ament_index_python.packages import get_package_share_directory
 
 
+import os
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.conditions import IfCondition, UnlessCondition
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import Command, LaunchConfiguration, PythonExpression
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
 
 from launch_ros.actions import Node
 
@@ -15,10 +20,49 @@ def generate_launch_description():
 
     package_name='vacuum_bot'
 
+    # Set the path to the Gazebo ROS package
+    pkg_gazebo_ros = FindPackageShare(package='gazebo_ros').find('gazebo_ros')   
+    
+    # Set the path to this package.
+    pkg_share = FindPackageShare(package='vacuum_bot').find('vacuum_bot')
+    
+    # Set the path to the world file
+    world_file_name = 'small_house.world'
+    world_path = os.path.join(pkg_share, 'worlds', world_file_name)
+    
+    # Set the path to the SDF model files.
+    gazebo_models_path = os.path.join(pkg_share, 'models')
+    os.environ["GAZEBO_MODEL_PATH"] = gazebo_models_path
+
+    headless = LaunchConfiguration('headless')
+    use_sim_time = LaunchConfiguration('use_sim_time')
+    use_simulator = LaunchConfiguration('use_simulator')
+    world = LaunchConfiguration('world')
+    
+    declare_simulator_cmd = DeclareLaunchArgument(
+        name='headless',
+        default_value='False',
+        description='Whether to execute gzclient')
+        
+    declare_use_sim_time_cmd = DeclareLaunchArgument(
+        name='use_sim_time',
+        default_value='true',
+        description='Use simulation (Gazebo) clock if true')
+    
+    declare_use_simulator_cmd = DeclareLaunchArgument(
+        name='use_simulator',
+        default_value='True',
+        description='Whether to start the simulator')
+    
+    declare_world_cmd = DeclareLaunchArgument(
+        name='world',
+        default_value=world_path,
+        description='Full path to the world model file to load')
+
     rsp = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory(package_name),'launch','rsp.launch.py'
-                )]), launch_arguments={'use_sim_time': 'true'}.items()
+                )]), launch_arguments={'use_sim_time': 'true', 'world': world}.items()
     )
 
     # Include the Gazebo launch file, provided by the gazebo_ros package
@@ -34,10 +78,15 @@ def generate_launch_description():
                         output='screen')
 
 
+    ld = LaunchDescription()
 
-    # Launch them all!
-    return LaunchDescription([
-        rsp,
-        gazebo,
-        spawn_entity,
-    ])
+    ld.add_action(declare_simulator_cmd)
+    ld.add_action(declare_use_sim_time_cmd)
+    ld.add_action(declare_use_simulator_cmd)
+    ld.add_action(declare_world_cmd)
+
+    ld.add_action(rsp)
+    ld.add_action(gazebo)
+    ld.add_action(spawn_entity)
+
+    return ld
